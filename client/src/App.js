6cbcf9dddd92c9a08f9f58b9e718ac1c1a6a730a
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-12-22 11:51:43 +0700

    fetch data from api

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,35 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import "./App.css";
 
+const base_url = process.env.REACT_APP_API_URL;
+
 function App() {
+  const [obj, setObj] = useState({});
+  const [sort, setSort] = useState({ sort: "rating", order: "desc" });
+  const [filterGenre, setFilterGenre] = useState([]);
+  const [page, setPage] = useState(1);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    const getAllMovies = async () => {
+      try {
+        const url = `${base_url}?page=${page}&sort=${sort.sort},${
+          sort.order
+        }&genre=${filterGenre.toString()}&search=${search}`;
+
+        const { data } = await axios.get(url);
+
+        setObj(data);
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getAllMovies();
+  }, [filterGenre, page, search, sort]);
+
   return <h1>Hello World</h1>;
 }
 
