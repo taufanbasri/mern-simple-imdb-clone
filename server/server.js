a31091052eae6402f96c0ca5d9b0cfb0308761b4
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-12-22 10:21:41 +0700

    Movie models & Routes

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const movieRoute = require("./routes/movies");
 const dbConnect = require("./dbConnect");
 const app = express();
 
@@ -9,6 +10,8 @@ dbConnect();
 
 app.use(express.json());
 app.use(cors());
+
+app.use("api", movieRoute);
 
 const port = process.env.PORT || 8080;
 
